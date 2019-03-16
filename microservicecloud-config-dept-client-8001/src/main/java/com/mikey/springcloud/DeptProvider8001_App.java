11b33e50f7ae13f7bdc2a46cc24027de06c577d7
mikeygithub <biaogejiushibiao@outlook.com>
2019-03-16 22:53:26 +0800

    配置中心

---
+++ microservicecloud-config-dept-client-8001/src/main/java/com/mikey/springcloud/DeptProvider8001_App.java
@@ -0,0 +1,24 @@
+package com.mikey.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Program: microservicecloud
+ * @Author: 麦奇
+ * @Email： 1625017540@qq.com
+ * @Create: 2019-03-16 21:49
+ * @Describe：
+ **/
+@SpringBootApplication
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
+@EnableDiscoveryClient //服务发现
+public class DeptProvider8001_App {
+    public static void main(String[] args){
+
+        SpringApplication.run(DeptProvider8001_App.class,args);
+
+    }
+}
